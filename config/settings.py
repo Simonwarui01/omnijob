eb55a9c93179f5691661c0656d254fbea3c49114
Simon Warui <simonwarui@Simons-MacBook-Pro.local>
2026-05-20 05:09:23 +0300

    Fix static files with whitenoise

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -98,3 +99,6 @@ CACHES = {
         'LOCATION': 'redis://localhost:6379/1',
     }
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pip install -r requirements.txt
+python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 gunicorn config.wsgi:application --bind 0.0.0.0:8000
