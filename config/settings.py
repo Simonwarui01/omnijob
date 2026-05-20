9a492e429d10c60260d313d0ab8956715d12d35c
Simon Warui <simonwarui@Simons-MacBook-Pro.local>
2026-05-20 06:26:57 +0300

    Fix Redis cluster with skip_full_coverage_check

--- config/settings.py
+++ config/settings.py
@@ -92,6 +92,7 @@ CELERY_RESULT_BACKEND = REDIS_URL
 CELERY_BROKER_TRANSPORT_OPTIONS = {
     'visibility_timeout': 3600,
     'socket_keepalive': True,
+    'skip_full_coverage_check': True,
 }
 if REDIS_URL.startswith('rediss://'):
     import ssl
