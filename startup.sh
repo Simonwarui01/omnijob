4ab9af19645e06f452b8cd7b3358960175204d46
Simon Warui <simonwarui@Simons-MacBook-Pro.local>
2026-05-21 06:05:46 +0300

    Remove pip install from startup for faster boot

--- startup.sh
+++ startup.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-celery -A config worker --loglevel=info --concurrency=32 -n worker1@%h &
-celery -A config worker --loglevel=info --concurrency=32 -n worker2@%h &
-celery -A config worker --loglevel=info --concurrency=32 -n worker3@%h &
-celery -A config worker --loglevel=info --concurrency=32 -n worker4@%h &
-# Only run beat on instance 0
-if [ "$WEBSITE_INSTANCE_ID" = "$(cat /home/site/wwwroot/.beat_instance 2>/dev/null)" ] || [ ! -f /home/site/wwwroot/.beat_instance ]; then
-    echo $WEBSITE_INSTANCE_ID > /home/site/wwwroot/.beat_instance
-    celery -A config beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
-fi
+celery -A config worker --loglevel=info --concurrency=8 -n worker1@%h &
+celery -A config worker --loglevel=info --concurrency=8 -n worker2@%h &
+celery -A config beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers=4
