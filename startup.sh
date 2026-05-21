cc8a42c4743569164c6e8d9d6d6184924126887e
Simon Warui <simonwarui@Simons-MacBook-Pro.local>
2026-05-21 05:59:33 +0300

    Scale to 4 Celery workers with 32 concurrency each

--- startup.sh
+++ startup.sh
@@ -2,7 +2,9 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-celery -A config worker --loglevel=info --concurrency=16 -n worker1@%h &
-celery -A config worker --loglevel=info --concurrency=16 -n worker2@%h &
+celery -A config worker --loglevel=info --concurrency=32 -n worker1@%h &
+celery -A config worker --loglevel=info --concurrency=32 -n worker2@%h &
+celery -A config worker --loglevel=info --concurrency=32 -n worker3@%h &
+celery -A config worker --loglevel=info --concurrency=32 -n worker4@%h &
 celery -A config beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
-gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers=8
+gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers=4
